96ff81359503f53294d9fa950747b43a8c84b631
RicardoEstrada2 <restrada_05@alu.uabcs.mx>
2023-11-02 20:37:25 -0700

    Ejercicio #4 'laravel para crear rutas'

---
+++ resources/views/user.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+</head>
+<body>
+    @if($name)
+        <h1>Hola {{ $name }} Doe</h1>
+    @endif
+    @if($lastName)
+        <h1>Hola {{ $name }} {{ $lastName }}</h1>
+    @endif
+    @if(!$name)
+        <h1>Hola Invitado</h1>
+    @endif
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat nisl eget turpis sollicitudin, sit amet scelerisque tellus malesuada. Suspendisse pulvinar a sapien mattis sagittis. Pellentesque eu sem dolor. Quisque tincidunt libero tortor, non rutrum leo viverra at. Mauris non fringilla lacus. Nunc hendrerit, enim in ornare accumsan, ex lorem vehicula mi, eget venenatis diam purus quis sem. Sed pulvinar lectus enim, id luctus ligula sollicitudin vel.</p>
+
+    <p>Cras sed turpis sed ligula porttitor luctus quis vel nulla. Fusce lobortis mi sed egestas vehicula. Integer dapibus diam sit amet tortor posuere, ac pretium augue maximus. Nunc elit nulla, convallis sit amet tincidunt nec, sollicitudin a purus. Nam erat erat, consequat sed sapien quis, porttitor posuere velit. Vivamus eleifend, ligula sit amet mollis tristique, leo odio convallis nisl, ut malesuada nunc leo et diam. Phasellus luctus sapien justo, non mattis lorem euismod non. Sed eu porta nisl, ac tincidunt enim. Quisque iaculis commodo semper. Ut sodales, tortor consectetur porttitor condimentum, metus justo vehicula dui, a interdum purus elit in nunc.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/operacion/{operation}/{num1}/{num2}', function (string $operation, int $num1, int $num2){
+    switch ($operation){
+        case 'suma':
+            return $num1 + $num2;
+        case 'multi':
+            return $num1 * $num2;
+        case 'division':
+            return $num1 / $num2;
+        case 'resta':
+            return $num1 - $num2;
+    }
+    return 'ingresa tu operacion: suma, rest, multiplicacion o division y los numeros';
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+Route::get('/user/{name?}/{lastName?}', function ($name = null,  $lastName = null) {
+    return view('user', ['name' => $name, 'lastName' => $lastName]);
+})->where(['name'=>'[A-Za-z', 'lastName'=>'[A-Za-z']);
+//Route::view('/user', 'user');
+
+
+Route::get('/multi/{num1}/{num2}', function (int $num1, int $num2){
+    return $num2 * $num1;
+});
